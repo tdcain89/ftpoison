7f7f0f9f753d98d06144dee63b57718e0051b175
Mat Trudel <mat@geeky.net>
2018-05-02 13:50:06 -0400

    Use elixir idiomatic return values because pid is hard to match on

--- lib/ftpoison/base.ex
+++ lib/ftpoison/base.ex
@@ -2,64 +2,65 @@ defmodule FTPoison.Base do
   defmacro __using__(_) do
     quote do
       @doc "Changes the working directory at the remote server to Dir."
-      @spec cd(PID, String.t) :: String.t
+      @spec cd(PID, String.t) :: :ok | {:error, {atom, atom}}
       def cd(pid, directory) do
         case :ftp.cd(pid, to_charlist(directory)) do
-          :ok -> pid
+          :ok -> :ok
           e -> handle_error(e)
         end
       end
 
       @doc "Starts a standalone FTP client process (without the Inets service framework)
       and opens a session with the FTP server at Host."
-      @spec open(String.t, map) :: {:ok, PID} | {:error, {atom, atom}}
+      @spec open(String.t, map) :: :ok
       def open(host, options \\ %{}) do
+        :ok
       end
 
       @doc "Returns the current working directory at the remote server."
-      @spec pwd(PID) :: String.t
+      @spec pwd(PID) :: {:ok, String.t} | {:error, {atom, atom}}
       def pwd(pid) do
         case :ftp.pwd(pid) do
-          {:ok, dir_char_list} -> to_string dir_char_list
+          {:ok, dir_char_list} -> {:ok, to_string(dir_char_list)}
           e -> handle_error(e)
         end
       end
 
       @doc "Returns a list of remote files in the current directory"
-      @spec list(PID) :: String.t
+      @spec list(PID) :: { :ok, [String.t]} | {:error, {atom, atom}}
       def list(pid) do
         case :ftp.nlist(pid) do
-          {:ok, dir_listing} -> to_string(dir_listing) |> String.split("\r\n", trim: true)
+          {:ok, dir_listing} -> {:ok, to_string(dir_listing) |> String.split("\r\n", trim: true)}
           e -> handle_error(e)
         end
       end
 
       @doc "Returns a list of remote files matching the specified path (supporting globs)"
-      @spec list(PID, String.t) :: String.t
+      @spec list(PID, String.t) :: {:ok, [String.t]} | {:error, {atom, atom}}
       def list(pid, path) do
         case :ftp.nlist(pid, to_charlist(path)) do
-          {:ok, dir_listing} -> to_string(dir_listing) |> String.split("\r\n", trim: true)
+          {:ok, dir_listing} -> {:ok, to_string(dir_listing) |> String.split("\r\n", trim: true)}
           e -> handle_error(e)
         end
       end
 
-      @spec recv(PID, String.t) :: {:ok, PID}
+      @spec recv(PID, String.t) :: :ok | {:error, {atom, atom}}
       def recv(pid, remote_file) do
         case :ftp.recv(pid, to_charlist(remote_file)) do
-          :ok -> pid
+          :ok -> :ok
           e -> handle_error(e)
         end
       end
 
-      @spec recv(PID, String.t, String.t) :: {:ok, PID}
+      @spec recv(PID, String.t, String.t) :: :ok | {:error, {atom, atom}}
       def recv(pid, remote_file, local_file) do
         case :ftp.recv(pid, to_charlist(remote_file), to_charlist(local_file)) do
-          :ok -> pid
+          :ok -> :ok
           e -> handle_error(e)
         end
       end
 
-      @spec recv_bin(PID, binary()) :: {:ok, binary()}
+      @spec recv_bin(PID, binary()) :: {:ok, binary()} | {:error, {atom, atom}}
       def recv_bin(pid, remote_file) do
         case :ftp.recv_bin(pid, to_charlist(remote_file)) do
           {:ok, contents} -> {:ok, contents}
@@ -67,34 +68,34 @@ defmodule FTPoison.Base do
         end
       end
 
-      @spec send(PID, String.t) :: {:ok, PID}
+      @spec send(PID, String.t) :: :ok | {:error, {atom, atom}}
       def send(pid, local_file) do
         case :ftp.send(pid, to_charlist(local_file)) do
-          :ok -> pid
+          :ok -> :ok
           e -> handle_error(e)
         end
       end
 
-      @spec send(PID, String.t, String.t) :: {:ok, PID}
+      @spec send(PID, String.t, String.t) :: :ok | {:error, {atom, atom}}
       def send(pid, local_file, remote_file) do
         case :ftp.send(pid, to_charlist(local_file), to_charlist(remote_file)) do
-          :ok -> pid
+          :ok -> :ok
           e -> handle_error(e)
         end
       end
 
-      @spec send_bin(PID, String.t, String.t) :: :ok
+      @spec send_bin(PID, String.t, String.t) :: :ok | {:error, {atom, atom}}
       def send_bin(pid, contents, remote_file) do
         case :ftp.send_bin(pid, contents, to_charlist(remote_file)) do
-          :ok -> pid
+          :ok -> :ok
           e -> handle_error(e)
         end
       end
 
-      @spec delete(PID, String.t) :: {:ok, PID}
+      @spec delete(PID, String.t) :: :ok | {:error, {atom, atom}}
       def delete(pid, remote_file) do
         case :ftp.delete(pid, to_charlist(remote_file)) do
-          :ok -> pid
+          :ok -> :ok
           e -> handle_error(e)
         end
       end
@@ -103,7 +104,7 @@ defmodule FTPoison.Base do
       def start(host) do
         start_inets()
         case :inets.start(:ftpc, host: to_charlist(host)) do
-          {:ok, pid} -> pid
+          {:ok, pid} -> {:ok, pid}
           e -> handle_error(e)
         end
       end
@@ -113,10 +114,10 @@ defmodule FTPoison.Base do
         :inets.stop(:ftpc, pid)
       end
 
-      @spec user(PID, String.t, String.t) :: {:ok, PID}
+      @spec user(PID, String.t, String.t) :: :ok | {:error, {atom, atom}}
       def user(pid, username, password) do
         case :ftp.user(pid, to_charlist(username), to_charlist(password)) do
-          :ok -> pid
+          :ok -> :ok
           e -> handle_error(e)
         end
       end
